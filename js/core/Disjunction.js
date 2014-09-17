f779813af5d796f72c1c62078299f03343500d37
Arcane Engineer <arcaneingenuity@gmail.com>
2014-09-17 19:46:31 +0200

    Changes missed from last commit.

---
+++ js/core/Disjunction.js
@@ -0,0 +1,33 @@
+Disjunction = function(timer, autoBuildAll, autoStartTimer)
+{
+	//classes
+	this.core = {};
+	this.extn = this.extensions = {};
+	
+	//instances
+	this.apps = {}; //anything not in this map will not be updated by timer.
+	this.appBuilder = new AppBuilder(this.apps);
+	
+	this.update = function(deltaSec)
+	{
+		for (var id in this.apps)
+		{	
+			var app = this.apps[id];
+			app.update(deltaSec);
+		}	
+	}
+	
+	this.timer = timer;
+	this.timer.callback = this.update.bind(this);
+	
+	if (autoBuildAll)
+	{
+		this.appBuilder.buildAll();
+	}
+	
+	if (autoStartTimer)
+	{
+		console.log(this.apps);
+		this.timer.start();
+	}
+}
--- js/extensions/core.js
+++ js/extensions/core.js
@@ -1,3 +1,13 @@
+function isNumber(n)
+{
+	return !isNaN(parseFloat(n)) && isFinite(n);
+}
+
+function isString(s)
+{
+	return typeof value == 'string' || value instanceof String;
+}
+
 //Messes with overridden .clone() methods -- in non-JS, these are all custom-defined anyway, so may as well do same.
 /*
 var clone = function() {
