56e424f099107bc40c02c4b1ef9702f2589a17ba
Arcane Engineer <arcaneingenuity@gmail.com>
2014-12-31 15:09:21 +0200

    Removed core directory. Renamed Disjunction.js to disjunction.js to match C version of library (and proper name of the library per se).

--- js/core/DeviceChannel.js
+++
@@ -1,7 +0,0 @@
-Disjunction.Core.DeviceChannel = function()
-{
-	this.value = 0;
-	this.delta = undefined;
-};
-if (disjunction.WINDOW_CLASSES) 
-	window.DeviceChannel = Disjunction.Core.DeviceChannel;
