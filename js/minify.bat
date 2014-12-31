ad8e19dae9e256a04424aa68b552b7997d6b92c3
Arcane Engineer <arcaneingenuity@gmail.com>
2014-12-31 15:20:22 +0200

    Adapted minification path and removed minified version from source control - it is left up to the user to run minify.bat or the equivalent uglifyjs command on their system.

---
+++ js/minify.bat
@@ -0,0 +1,2 @@
+uglifyjs disjunction.js -o ../js/disjunction-core.min.js
+REM --source-map ../disjunction-core.min.js.map
--- js/uglify.bat
+++
@@ -1,4 +0,0 @@
-cd core
-uglifyjs Disjunction.js App.js Builder.js Ctrl.js Device.js DeviceChannel.js DeviceHub.js Model.js Phase.js Phaser.js Point2.js Pointer.js ServiceHub.js Timeline.js Timer.js View.js -o ../build/disjunction-core.min.js
-cd ..
-REM --source-map ../build/disjunction-core.min.js.map
