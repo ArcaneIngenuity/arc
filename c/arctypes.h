8971c7dfa425be899fd58253f90ac689418efe4f
Arcane Engineer <arcaneingenuity@gmail.com>
2015-09-23 19:23:12 +0200

    Detection of size_t under linux.

--- c/arctypes.h
+++ c/arctypes.h
@@ -1,6 +1,7 @@
 #ifndef ARCTYPES_H
 #define ARCTYPES_H
 #include <stdint.h>
+#include <stddef.h>
 void * 		addressofDynamic(const char * name);
 size_t 		sizeofDynamic(const char * name);
 #endif// ARCTYPES_H
